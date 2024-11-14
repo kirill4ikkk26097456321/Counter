1e01093534de7c136d21f033b68afeebc45206f4
KirillK <k.komarnitsky@ya.ru>
2024-11-14 22:37:58 +0300

    Создан проект

--- Counter1/ViewController.swift
+++ Counter1/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var AddButton: UIButton!
+    @IBOutlet weak var FirstImageView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        AddButton.tintColor = .red
     }
-
-
+    private var c: Int = 0
+    @IBAction func Addition(_ sender: Any) {
+        c += 1
+        FirstImageView.text = String(c)
+    }
+    
+    @IBAction func NotAddButton(_ sender: Any) {
+        if c > 0 {
+            c -= 1
+            FirstImageView.text = String(c)
+        }
+    }
+    @IBAction func MakeZeroButton(_ sender: Any) {
+        c = 0
+        FirstImageView.text = String(c)
+    }
 }
-
